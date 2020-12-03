49387e382ce04d80db0c6cd126cdd2fbaac62ebc
XiaoTong <2844893235@one.com>
2020-12-03 16:19:26 +0800

    2020.12.03完成重复密码,切换注册登录功能2.0版本

--- src/utils/vaildatas.js
+++ src/utils/vaildatas.js
@@ -2,8 +2,13 @@
     用来过滤特殊字符
 */
 
-export const validatas_inputValue = s =>{
-    var pattern = new RegExp("[`%~!@#$^&*()=|{}':;',\\[\\].<>/?~！@#￥……&*（）&mdash;—|{}【】‘；：”“'。，、？]")
+const validatas_inputValue =  (s,type) =>{
+    if(type=="email"){
+         var pattern = new RegExp("[`%~!#$^&*()=|{}':;',\\[\\]<>/?~！#￥……&*（）&;—|{}【】‘；：”“'。，、？]")
+    }else{
+        var pattern = new RegExp("[`%~!@#$^&*()=|{}':;',\\[\\].<>/?~！@#￥……&*（）&;—|{}【】‘；：”“'。，、？]")
+    }
+    
     var rs = "";
     for (var i = 0; i < s.length; i++) {
             rs = rs + s.substr(i, 1).replace(pattern, '');
@@ -11,4 +16,28 @@ export const validatas_inputValue = s =>{
     return rs;
 }
 
-    
+//验证邮箱
+const test_email = (value) =>{
+    let reg = /^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i
+    return ! reg.test(value)
+}
+//验证密码
+const test_password =(value)=>{
+    let reg = /^(?!\D+$)(?![^a-zA-Z]+$)\S{6,20}$/
+    return ! reg.test(value)
+}
+//验证验证码
+const test_code =(value)=>{
+    let reg = /^[a-z0-9]{6}$/;//单一的数字或者字母
+    return ! reg.test(value)
+} 
+
+export default{
+    validatas_inputValue,
+    test_email,
+    test_password,
+    test_code
+}
+
+
+
--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -15,12 +15,17 @@
             <label for="password" class="menu_name">密码</label>
             <el-input id="password" type="text" v-model="ruleForm.password" autocomplete="off"></el-input>
           </el-form-item>
+
+          <el-form-item v-if = 'mode =="register" '  prop="password1" class="from_item" >
+            <label for="password1" class="menu_name">重复密码</label>
+            <el-input id="password1" type="text" v-model="ruleForm.password1" autocomplete="off"></el-input>
+          </el-form-item>
           
           <el-form-item prop="code" class="from_item"> 
               <label for="code" class="menu_name" >验证码</label>
               <el-row :gutter="20" prop="age" class="from_item">
                 <el-col :span="15">
-                    <el-input id="code" v-model.number="ruleForm.code"></el-input>
+                    <el-input id="code" v-model="ruleForm.code"></el-input>
                 </el-col>
                 <el-col :span="9">
                     <el-button type="success" class="black">获取验证码</el-button>
@@ -29,7 +34,7 @@
           </el-form-item>
 
           <el-form-item >
-            <el-button type="danger" @click="submitForm('ruleForm')" class="black">提交</el-button>
+            <el-button type="danger" @click="submitForm('ruleForm')" class="black">{{this.mode=="login" ? "登录":"注册"}}</el-button>
           </el-form-item>
         </el-form>
 
@@ -40,31 +45,19 @@
 
 
 <script>
-import {validatas_inputValue} from "@/utils/vaildatas.js"
+import dataobject from "@/utils/vaildatas.js"
 
 export default {
   name: "login",
   data(){
     //Rule验证的字段  value 输入的值   callback验证后要做什么(回调函数)
-    //验证码
-    var validateCode = (rule, value, callback) => {
-      console.log(value)
-      console.log(validatas_inputValue(value))
-        let reg = /^[a-z0-9]{6}$/;//单一的数字或者字母
-        if (!value) {
-          return callback(new Error('请输入验证码'));
-        }else if(!reg.test(value)){
-          return callback(new Error('请输入单一的6位数字或者字母'));
-        }else{
-          callback();
-        }
-    };
     //邮箱
     var validateUsername = (rule, value, callback) => {
-         let reg = /^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i
+      //过滤
+        this.ruleForm.username = value = dataobject.validatas_inputValue(value,"email")
         if (value === '') {
           callback(new Error('请输入邮箱'));
-        } else if(!reg.test(value)) {
+        } else if(dataobject.test_email(value)) {
           callback(new Error('邮箱格式错误'));
         }else{
           callback();
@@ -72,24 +65,59 @@ export default {
     };
     // 密码
     var validatePassword = (rule, value, callback) => {
-        let reg = /^(?!\D+$)(?![^a-zA-Z]+$)\S{6,20}$/
+      //过滤
+        this.ruleForm.password = value = dataobject.validatas_inputValue(value,"password")
+        
         if (value === '') {
           callback(new Error('请输入密码'));
-        }else if(!reg.test(value)) {
+        }else if(dataobject.test_password(value)) {
           callback(new Error('请输入数字+字母的格式'));
         }else{
            callback();
         }
     };
+
+   //重复密码
+    var validatePassword1 = (rule, value, callback) => {
+      //过滤
+        this.ruleForm.password1 = value = dataobject.validatas_inputValue(value,"password1")
+        
+        if (value !== this.ruleForm.password) {
+          callback(new Error('两次密码输入不一致'));
+        }else{
+           callback(); 
+        }
+    };
+
+
+    //验证码
+    var validateCode = (rule, value, callback) => {
+      //过滤
+        this.ruleForm.code = value = dataobject.validatas_inputValue(value,"code")
+        if (!value) {
+          return callback(new Error('请输入验证码'));
+        }else if(dataobject.test_code(value)){
+          return callback(new Error('请输入单一的6位数字或者字母'));
+        }else{
+          callback();
+        }
+    };
+
+
+
+
     return{
+      mode:"login",
       mentTab:[
-        {txt:"登录",current:false},
-        {txt:"注册",current:true}
+        {txt:"登录",current:true,type:"login"},
+        {txt:"注册",current:false,type:"register"}
       ],
+   
       //表单的双向绑定数据
       ruleForm: {
         username: '',
         password: '',
+        password1: '',
         code: ''
       },
       //验证规则 校验方式
@@ -99,6 +127,9 @@ export default {
         ],
         password: [
           { validator: validatePassword, trigger: 'blur' }
+        ],
+        password1: [
+          { validator: validatePassword1, trigger: 'blur' }
         ],
         code: [
           { validator: validateCode, trigger: 'blur' }
@@ -113,6 +144,7 @@ export default {
       //对表单的每一个字段进行验证
       this.mentTab.map(item => item.current =false)
       item.current=true;
+      this.mode=item.type//点击的时候让重复密码出来
     },
     submitForm(formName) {
       this.$refs[formName].validate((valid) => {
